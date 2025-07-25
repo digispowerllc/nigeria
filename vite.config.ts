48700d995966561f84285929ba7db62cb42a5fb4
OgwoGP <digispowerllc@gmail.com>
2025-07-25 13:26:35 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		host: true, // or use a specific IP like '192.168.0.14'
+		port: 5173, // optional
+	},
 	plugins: [tailwindcss(), sveltekit()]
 });
